3c011c706d190b4dab2b597950022c33dd52e376
sombaesler <sombaesler@gmail.com>
2021-11-27 10:21:03 -0800

    covert to intercept method

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -4,27 +4,26 @@ describe('Test with backend', () => {
 
     beforeEach('log in to the app', () => {
 
-        cy.server()
-        cy.route('GET', '**/tags', 'fixture:tags.json')
+        cy.intercept({method: 'GET', path: 'tags'}, {fixture: 'tags.json'})
         cy.loginToApplication()
 
     })
 
     it('verify correct request and response', () => {
-        cy.server()
-        cy.route('POST', '**/articles').as('postArticles')
+
+        cy.intercept({method: 'POST', path: 'articles', url: 'https://api.realworld.io/api/articles/'}).as('postArticles')
 
         cy.contains('New Article').click()
-        cy.get('[formcontrolname="title"]').type('This is a title5')
+        cy.get('[formcontrolname="title"]').type('This is a title')
         cy.get('[formcontrolname="description"]').type('This is description')
         cy.get('[formcontrolname="body"]').type('This is a body')
         cy.contains('Publish Article').click()
+        
 
-        cy.wait('@postArticles')
-        cy.get('@postArticles').then(xhr => {
+        cy.wait('@postArticles').then((xhr) => {
             console.log(xhr)
-            expect(xhr.status).to.equal(200)
             expect(xhr.request.body.article.body).to.equal('This is a body')
+            expect(xhr.response.statusCode).to.equal(200)
             expect(xhr.response.body.article.description).to.equal('This is description')
         })
     })
@@ -37,8 +36,8 @@ describe('Test with backend', () => {
     })
 
     it('verify global feed likes count', () => {
-        cy.route('GET', '**/articles/feed*', '{"articles":[],"articlesCount":0}')
-        cy.route('GET', '**/articles*', 'fixture:articles.json')
+        cy.intercept('GET', '**/articles/feed*', {"articles":[],"articlesCount":0})
+        cy.intercept('GET', '**/articles*', {fixture: 'articles.json'})
 
         cy.contains('Global Feed').click()
         cy.get('app-article-list button').then(listOfButtons => {
@@ -48,7 +47,7 @@ describe('Test with backend', () => {
 
         cy.fixture('articles').then(file => {
             const articleLink = file.articles[1].slug
-            cy.route('POST', '**/articles/' + articleLink + 'favorite', file)
+            cy.intercept('POST', '**/articles/' + articleLink + 'favorite', file)
         })
 
         cy.get('app-article-list button')
